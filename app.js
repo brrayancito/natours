33bbae7548c38d79204d53c7ff0a381869a6efa8
Brrayan Javy <brrayanperez10@gmail.com>
2023-04-07 10:26:44 -0400

    Handling PATCH request, added app.patch

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.get('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+// Create new tour 🟨
 app.post('/api/v1/tours', (req, res) => {
   // console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
@@ -59,6 +60,23 @@ app.post('/api/v1/tours', (req, res) => {
     });
   });
   // res.send('Done');
+});
+
+// Update tour 🟨
+app.patch('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length - 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'Updated your here...',
+    },
+  });
 });
 
 app.listen(port, () => {
