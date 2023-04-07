72d8095c62d9bb58ce1cd5a842d0fb3a6c714a18
Brrayan Javy <brrayanperez10@gmail.com>
2023-04-07 10:40:56 -0400

    Handling DELETE request

--- app.js
+++ app.js
@@ -79,6 +79,23 @@ app.patch('/api/v1/tours/:id', (req, res) => {
   });
 });
 
+// Delete tour 🟨
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (+req.params.id > tours.length - 1) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: {
+      tour: null,
+    },
+  });
+});
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
