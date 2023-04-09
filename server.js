93fba0696cf64086a92f5b86a2bffbf0c685e523
Brrayan Javy <brrayanperez10@gmail.com>
2023-04-09 14:17:26 -0400

    Added some code to .eslint

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-
 const app = require('./app.js');
+
+dotenv.config({ path: './config.env' });
 
 // console.log(process.env);
 // console.log(app.get('env'));
