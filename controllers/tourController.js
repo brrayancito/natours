bacd728e1c2bd93af68b5f6e517a0fd5d7eb906c
Brrayan Javy <brrayanperez10@gmail.com>
2023-04-12 16:28:32 -0400

    Updating documents, using findByIdAndUpdate

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -63,13 +63,25 @@ exports.createTour = async (req, res) => {
 };
 
 //------------------------ Update Tour 🟨
-exports.updateTour = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour: 'Updated your here...',
-    },
-  });
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent!',
+    });
+  }
 };
 
 //------------------------ Delete Tour 🟨
