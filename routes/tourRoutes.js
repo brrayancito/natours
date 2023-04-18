2fe4d98856782497dde4b248dd9091578580abc6
Brrayan Javy <brrayanperez10@gmail.com>
2023-04-18 17:22:08 -0400

    Aggregation Piperline: Unwinding and Projecting

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -7,9 +7,11 @@ const router = express.Router();
 //----------------- Tour Routes 🟨
 router.route('/top-5-cheap').get(tourController.aliasTopTours, tourController.getAllTours);
 
-router.route('/').get(tourController.getAllTours).post(tourController.createTour);
-
 router.route('/tour-stats').get(tourController.getTourStats);
+
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
+router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 
 router
   .route('/:id')
