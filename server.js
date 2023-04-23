e2736c545e136d93ca6315fb3f2062242c1ce0c2
Brrayan Javy <brrayanperez10@gmail.com>
2023-04-23 13:59:50 -0400

    Catching Uncaught Exceptions

--- server.js
+++ server.js
@@ -3,6 +3,14 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app.js');
 
+// ------------------- Uncaught Exceptions
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTIONS! Shutting Down...');
+  console.log(err.name, err.message);
+
+  process.exit(1);
+});
+
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 //Connecting to MongoDB Atlas DATABASE
@@ -21,9 +29,10 @@ const server = app.listen(port, () => {
   console.log(process.env.NODE_ENV);
 });
 
+// ------------------- Unhandled Rejections
 process.on('unhandledRejection', (err) => {
-  console.log(err.name, err.message);
   console.log('UNHANDLED REJECTION! Shutting Down...');
+  console.log(err.name, err.message);
 
   server.close(() => {
     process.exit(1);
