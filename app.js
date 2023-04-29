47733b9c80b010a443307e62fd567a8823836df7
Brrayan Javy <brrayanperez10@gmail.com>
2023-04-29 16:39:39 -0400

    Preventing Parameter Pollution

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError.js');
 const globalErrorHandler = require('./controllers/errorController.js');
@@ -16,6 +19,26 @@ app.use(helmet());
 
 //Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
+
+//Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+//Data sanitization against XSS
+app.use(xss());
+
+//Prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsAverage',
+      'ratingsQuantity',
+      'maxGroupSize',
+      'price',
+      'difficulty',
+    ],
+  })
+);
 
 //Serving Static files
 app.use(express.static(`${__dirname}/public`));
