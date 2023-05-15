a3606e521159324f8c24cf3bbd7358035f36cafa
Brrayan Javy <brrayanperez10@gmail.com>
2023-05-15 16:28:17 -0400

    Including a Map with leaflet - No longer use Mapbox

--- app.js
+++ app.js
@@ -25,8 +25,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static(`${__dirname}/public`));
 
 //Set Security HTTP headers
-app.use(helmet());
 
+// Further HELMET configuration for Security Policy (CSP)
+const scriptSrcUrls = ['https://unpkg.com/', 'https://tile.openstreetmap.org'];
+const styleSrcUrls = [
+  'https://unpkg.com/',
+  'https://tile.openstreetmap.org',
+  'https://fonts.googleapis.com/',
+];
+const connectSrcUrls = ['https://unpkg.com', 'https://tile.openstreetmap.org'];
+const fontSrcUrls = ['fonts.googleapis.com', 'fonts.gstatic.com'];
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: [],
+      connectSrc: ["'self'", ...connectSrcUrls],
+      scriptSrc: ["'self'", ...scriptSrcUrls],
+      styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+      workerSrc: ["'self'", 'blob:'],
+      objectSrc: [],
+      imgSrc: ["'self'", 'blob:', 'data:', 'https:'],
+      fontSrc: ["'self'", ...fontSrcUrls],
+    },
+  })
+);
 //Body parser, reading data from the body into req.body
 app.use(express.json({ limit: '10kb' }));
 
--- public/js/mapbox.js
+++
@@ -1,5 +0,0 @@
-/* eslint-disable */
-
-console.log('Hello from the client side!');
-const locations = JSON.parse(document.getElementById('map').dataset.locations);
-console.log(locations);
