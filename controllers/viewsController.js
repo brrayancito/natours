e94bfa54cdab49275042af8c4e9fd2e93a38e91f
Brrayan Javy <brrayanperez10@gmail.com>
2023-05-15 19:43:18 -0400

    Building the Login Screen

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -10,19 +10,25 @@ exports.getOverview = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) Get the data for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
     fields: 'review rating user',
   });
 
-  console.log(tour);
+  //   console.log(tour);
   // 2) Build templete
 
   // 2) Render template using data from step 1
   res.status(200).render('tour', {
     title: `${tour.name} Tour`,
     tour,
+  });
+});
+
+exports.getLoginForm = catchAsync(async (req, res) => {
+  res.status(200).render('login', {
+    title: `Log into your account`,
   });
 });
